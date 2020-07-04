65d8c1233603d42c65fe3b9dbf0f6e8652a6e73a
CocoHuissoon <chuissoon@hotmail.com>
2020-07-04 22:34:48 +0200

    for my love

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          For my lovee
         </a>
       </header>
     </div>
