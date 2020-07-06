ba6f689f7bf9429dfb8161c1a0571359d837cddc
chuis <chuissoon@hotmail.com>
2020-07-06 21:52:11 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import heart from './heart.svg';
 import './App.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
-  return (
+    const [message, setMessage] = useState("I love you more!");
+
+
+    function changeMessage() {
+        setMessage("Yes I really really do <3");
+    }
+    return (
     <div className="App">
       <header className="App-header">
         <img src={heart} className="App-logo" alt="logo" />
-        <h1>I love you more!</h1>
+        <h1>{message}</h1>
+
+          <Button variant="success"
+                  size={"lg"}
+                  onClick={() => changeMessage()}>No you don't!</Button>
       </header>
     </div>
   );
