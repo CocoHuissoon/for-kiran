6adc27b106313a94f6db0450e2937a10d1718440
chuis <chuissoon@hotmail.com>
2020-07-06 22:07:27 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -10,18 +10,23 @@ function App() {
     function changeMessage() {
         setMessage("Yes I really really do <3");
     }
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={heart} className="App-logo" alt="logo" />
-        <h1>{message}</h1>
 
-          <Button variant="success"
-                  size={"lg"}
-                  onClick={() => changeMessage()}>No you don't!</Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={heart} className="App-logo" alt="logo"/>
+                <h1>{message}</h1>
+
+                <Button
+                    hidden={message == "Yes I really really do <3"}
+                    variant="success"
+                    size={"lg"}
+                    onClick={() => {
+                        changeMessage()
+                    }}>No you don't!</Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
